abf38ac1cd8cb4b02743883d6771b08dfffeb4ac
HirenPithva <hirenpithva150@gmail.com>
2025-06-01 16:31:25 +0530

    Added few components to understand the emulated - none - shadowDom

---
+++ src/view-encapsulation/root-component/node-component/node-component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'node-component',
+  imports: [],
+  templateUrl: './node-component.html',
+  styleUrl: './node-component.css',
+  encapsulation: ViewEncapsulation.ShadowDom
+})
+export class NodeComponent {
+
+}
